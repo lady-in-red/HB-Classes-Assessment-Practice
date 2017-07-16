0b1c66daff8c42208fd3374b4065d1c709b91db6
vagrant <vagrant@vagrant.vm>
2017-07-16 05:38:23 +0000

    WIP oo assessment practice.

---
+++ practice.py
@@ -0,0 +1,8 @@
+"""
+Parts 1-4
+Create your classes and class methods here according to the practice instructions.
+As you are working on Parts 1, 2, and 4, you can run the test python file
+corresponding to that section to verify that you are completing the problem
+correctly.
+ex: python part_1_tests.py.
+"""
